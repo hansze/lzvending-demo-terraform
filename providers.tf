2b940526b2b3c12cf04981d1f2fd0b76eb049a75
hansze <hansze@microsoft.com>
2023-08-14 21:31:25 +0200

    Changed subscription_id

--- providers.tf
+++ providers.tf
@@ -1,16 +1,16 @@
 provider "azurerm" {
   features {}
-  subscription_id = "0a8501c6-55c7-4826-9252-9823c6f839a1"
+  subscription_id = "660f8f6b-518b-4d71-ba96-c59beab19f3a"
 }
 
 provider "azurerm" {
   features {}
   alias           = "management"
-  subscription_id = "0a8501c6-55c7-4826-9252-9823c6f839a1"
+  subscription_id = "660f8f6b-518b-4d71-ba96-c59beab19f3a"
 }
 
 provider "azurerm" {
   features {}
   alias           = "connectivity"
-  subscription_id = "8b0bdc29-f686-4d2b-aa3f-4c2b509af786"
+  subscription_id = "660f8f6b-518b-4d71-ba96-c59beab19f3a"
 }

1dfb6cf3c547df0c03368e90fe81bc6d340d7a54
hansze <hansze@microsoft.com>
2023-08-17 11:16:16 +0200

    Added vWAN Example settings

--- main.alz.tf
+++ main.alz.tf
@@ -37,7 +37,7 @@ module "alz" {
   // Set subscription IDs for placement of platform subs
   subscription_id_management   = data.azurerm_client_config.management.subscription_id
   subscription_id_connectivity = data.azurerm_client_config.connectivity.subscription_id
-  subscription_id_identity     = "660f8f6b-518b-4d71-ba96-c59beab19f3a"
+  subscription_id_identity     = "26f87617-364a-4ac7-bdc3-44bf3e33b253" # ME-M365x12435175-hansze-2
 
   // Use management group association instead of having to be explicit about MG membership
   strict_subscription_association = false
@@ -61,31 +61,142 @@ module "alz" {
 
   // Connectivity (hub network) configuration
   deploy_connectivity_resources = true
+  # Configure custom settings for the module to deploy Virtual WAN hub
+  # network resources instead of traditional hub network resources.
+  # Below taken from https://github.com/Azure/terraform-azurerm-caf-enterprise-scale/wiki/%5BExamples%5D-Deploy-Virtual-WAN-Resources
   configure_connectivity_resources = {
     settings = {
+      hub_networks = []
+      vwan_hub_networks = [
+        {
+          enabled = true
+          config = {
+            address_prefix = "10.200.0.0/22"
+            location       = ""
+            sku            = ""
+            routes         = []
+            expressroute_gateway = {
+              enabled = false
+              config = {
+                scale_unit = 1
+              }
+            }
+            vpn_gateway = {
+              enabled = false
+              config = {
+                bgp_settings       = []
+                routing_preference = ""
+                scale_unit         = 1
+              }
+            }
+            azure_firewall = {
+              enabled = false
+              config = {
+                enable_dns_proxy              = false
+                dns_servers                   = []
+                sku_tier                      = "Standard"
+                base_policy_id                = ""
+                private_ip_ranges             = []
+                threat_intelligence_mode      = ""
+                threat_intelligence_allowlist = []
+                availability_zones = {
+                  zone_1 = true
+                  zone_2 = true
+                  zone_3 = true
+                }
+              }
+            }
+            spoke_virtual_network_resource_ids        = []
+            secure_spoke_virtual_network_resource_ids = []
+            enable_virtual_hub_connections            = false
+          }
+        },
+      ]
       ddos_protection_plan = {
         enabled = false
+        config = {
+          location = ""
+        }
       }
       dns = {
-        enabled = false
-      }
-      hub_networks = [{
-        config = {
-          address_space = ["10.0.0.0/23"]
-          azure_firewall = {
-            enabled = false
-          }
-          subnets = [{
-            address_prefixes = ["10.0.0.0/24"]
-            name             = "default"
-          }]
-          virtual_network_gateway = {
-            enabled = false
-          }
-        }
         enabled = true
-      }]
+        config = {
+          location = ""
+          enable_private_link_by_service = {
+            azure_api_management                 = true
+            azure_app_configuration_stores       = true
+            azure_arc                            = true
+            azure_automation_dscandhybridworker  = true
+            azure_automation_webhook             = true
+            azure_backup                         = true
+            azure_batch_account                  = true
+            azure_bot_service_bot                = true
+            azure_bot_service_token              = true
+            azure_cache_for_redis                = true
+            azure_cache_for_redis_enterprise     = true
+            azure_container_registry             = true
+            azure_cosmos_db_cassandra            = true
+            azure_cosmos_db_gremlin              = true
+            azure_cosmos_db_mongodb              = true
+            azure_cosmos_db_sql                  = true
+            azure_cosmos_db_table                = true
+            azure_data_explorer                  = true
+            azure_data_factory                   = true
+            azure_data_factory_portal            = true
+            azure_data_health_data_services      = true
+            azure_data_lake_file_system_gen2     = true
+            azure_database_for_mariadb_server    = true
+            azure_database_for_mysql_server      = true
+            azure_database_for_postgresql_server = true
+            azure_digital_twins                  = true
+            azure_event_grid_domain              = true
+            azure_event_grid_topic               = true
+            azure_event_hubs_namespace           = true
+            azure_file_sync                      = true
+            azure_hdinsights                     = true
+            azure_iot_dps                        = true
+            azure_iot_hub                        = true
+            azure_key_vault                      = true
+            azure_key_vault_managed_hsm          = true
+            azure_kubernetes_service_management  = true
+            azure_machine_learning_workspace     = true
+            azure_managed_disks                  = true
+            azure_media_services                 = true
+            azure_migrate                        = true
+            azure_monitor                        = true
+            azure_purview_account                = true
+            azure_purview_studio                 = true
+            azure_relay_namespace                = true
+            azure_search_service                 = true
+            azure_service_bus_namespace          = true
+            azure_site_recovery                  = true
+            azure_sql_database_sqlserver         = true
+            azure_synapse_analytics_dev          = true
+            azure_synapse_analytics_sql          = true
+            azure_synapse_studio                 = true
+            azure_web_apps_sites                 = true
+            azure_web_apps_static_sites          = true
+            cognitive_services_account           = true
+            microsoft_power_bi                   = true
+            signalr                              = true
+            signalr_webpubsub                    = true
+            storage_account_blob                 = true
+            storage_account_file                 = true
+            storage_account_queue                = true
+            storage_account_table                = true
+            storage_account_web                  = true
+          }
+          private_link_locations                                 = []
+          public_dns_zones                                       = []
+          private_dns_zones                                      = []
+          enable_private_dns_zone_virtual_network_link_on_hubs   = true
+          enable_private_dns_zone_virtual_network_link_on_spokes = true
+          virtual_network_resource_ids_to_link                   = []
+        }
+      }
     }
-    tags = null
+    location = null
+    tags     = null
+    advanced = null
   }
 }

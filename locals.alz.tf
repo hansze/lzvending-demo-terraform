07cd70b9276461762b33a0e6f2fd2632799d9da9
hansze <hansze@microsoft.com>
2023-08-21 19:38:21 +0200

    Remove vWAN connection

--- locals.alz.tf
+++ locals.alz.tf
@@ -1,7 +1,8 @@
-#locals {
-#  hub_networks_by_location = {
-#    for k, v in module.alz.azurerm_virtual_network.connectivity :
-#    v.location => k
-#  }
-#}
-#
+locals {
+  hub_networks_by_location = {
+    for k, v in module.alz.azurerm_virtual_network.connectivity :
+    v.location => k
+  }
+}
+
+

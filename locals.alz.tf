2c3cf5c294e60c457a2b7ff31bb5189004c3688a
hansze <hansze@microsoft.com>
2023-08-21 18:21:30 +0200

    More troubleshooting

--- locals.alz.tf
+++ locals.alz.tf
@@ -1,6 +1,7 @@
-locals {
-  hub_networks_by_location = {
-    for k, v in module.alz.azurerm_virtual_network.connectivity :
-    v.location => k
-  }
-}
+#locals {
+#  hub_networks_by_location = {
+#    for k, v in module.alz.azurerm_virtual_network.connectivity :
+#    v.location => k
+#  }
+#}
+#

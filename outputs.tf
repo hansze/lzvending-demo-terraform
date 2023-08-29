290eaf3ce415450902abbbb2f33d9b3565d5b82c
hansze <hansze@microsoft.com>
2023-08-29 20:27:35 +0200

    change to outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,6 @@
 data "azurerm_client_config" "current" {
 }
 
-output "c" {
-  description = "Connectivity Object"
-  value       = data.azurerm_client_config.connectivity.this.virtual_networks
-}
-
-
 output "account_id" {
   value = data.azurerm_client_config.current.client_id
 }
